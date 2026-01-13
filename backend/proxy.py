43ae104e53dd95e581dec607f99d41299441c4f2
rdhhauzan <muhammad.hauzan@binus.ac.id>
2026-01-13 13:15:08 +0700

    feat: add track and stop proxy

--- backend/proxy.py
+++ backend/proxy.py
@@ -1,10 +1,14 @@
 import random
+import signal
 import subprocess
 
 proxies = {}
 
 
 def start_proxy(conn):
+    if conn["id"] in proxies:
+        return proxies[conn["id"]]["port"]
+
     ws_port = random.randint(20000, 30000)
 
     proc = subprocess.Popen(
@@ -14,3 +18,18 @@ def start_proxy(conn):
     proxies[conn["id"]] = {"port": ws_port, "proc": proc}
 
     return ws_port
+
+
+def stop_proxy(conn_id):
+    info = proxies.get(conn_id)
+    if not info:
+        return
+
+    proc = info["proc"]
+
+    try:
+        proc.terminate()
+    except Exception:
+        pass
+
+    proxies.pop(conn_id, None)
